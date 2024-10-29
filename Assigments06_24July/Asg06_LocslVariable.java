e0891e3bb48e558ec711264803533e2ebec61941
SandeepKintel <Sandeepkr110295@gmail.com>
2024-10-30 00:52:42 +0530

    updating directoy

---
+++ Assigments06_24July/Asg06_LocslVariable.java
@@ -0,0 +1,17 @@
+package Assigments06_24July;
+
+public class Asg06_LocslVariable {
+	static void show() {
+		int a, b;
+		a=10;
+		b=2;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+	
+	public static void main(String[] args) {
+		Asg06_LocslVariable.show();
+	    int sum2=a+b;
+	}
+
+}
